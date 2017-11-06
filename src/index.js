885f30a0a890e61b3e26c10e974b923f28beb88e
MITRAIS\Ramdony_R2455 <RamdonyAriesta.Rindiawan@mitrais.com>
2017-11-06 12:30:37 +0700

    adding comment and fixing update on changed props

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ import PropTypes from 'prop-types';
 class Form extends Component{
 
     componentWillMount(){
+        this.modifyChild(this.props);
+    }
+
+    componentDidMount(){
+        const dataValue = ef.getInputsValue(this.props.children);
+        this.setDataValue(dataValue);
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.modifyChild(nextProps);
+    }
+
+
+    /*
+    find and modify the input type children by adding an event listener
+    */
+    modifyChild(props){
         const children = ef.modifyInputs((child, key) => {
             if(
                 child.type === 'button'
@@ -15,7 +32,8 @@ class Form extends Component{
                         child.props.type === 'submit'
                         || child.props.type === 'button'
                     )
-                )){
+                )
+            ){
                 return React.cloneElement(child, {
                     key: key,
                     onClick: this.handler.bind(this)
@@ -26,24 +44,30 @@ class Form extends Component{
                     onChange: this.handler.bind(this)
                 })
             }
-        }, this.props.children);
+        }, props.children);
         this.setState({children});
     }
 
-    componentDidMount(){
-        const dataValue = ef.getInputsValue(this.props.children);
-        this.setDataValue(dataValue);
-
-    }
-
+    /*
+    store updated input values to the state and execute stateSetter to sync
+    parent's component state
+    @dataValue : updated input value
+    */
     setDataValue(dataValue){
         this.setState({dataValue});
         this.props.stateSetter(dataValue);
     }
 
+    /*
+    handle onChange event to get updated value and store it in the input values
+    collection
+    @e : event
+    */
     handler(e){
         let {dataValue} = this.state;
         const eName = e.currentTarget.name;
+
+        // ensure the target have a name attribute
         if(typeof(eName) !== 'undefined' && eName!==''){
             dataValue[eName] = ef.getTargetDataValue(e, dataValue[eName]);
             this.setDataValue(dataValue);
